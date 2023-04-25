23e8e9e2dbc9e160a3406b4e8dc9a94afd12f891
DESKTOP-HHMVOCR\JeongYoon <kjyoon811@gmail.com>
2023-04-26 00:02:43 +0900

    2023-04-26

--- src/main/java/edu/kh/comm/common/filter/InitFilter.java
+++ src/main/java/edu/kh/comm/common/filter/InitFilter.java
@@ -16,54 +16,47 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-@WebFilter(filterName="initFilter", urlPatterns="/*")
+@WebFilter(filterName="initFilter", urlPatterns = "/*")
 public class InitFilter extends HttpFilter implements Filter {
+	
 	
 	// print 구문 사용하다 걸리면 바보
 	// -> Logger 사용
 	
-	// Logger 객체 생성(해당 클래스에 대한 log를 출력하는 객체)
-	
+	// Logger 객체 생성 (해당 클래스에 대한 log를 출력하는 객체)
 	private Logger logger = LoggerFactory.getLogger(InitFilter.class);
 	
-	// 필터가 생성될 때 실행	
+    // 필터가 생성될 때 실행   
 	public void init(FilterConfig fConfig) throws ServletException {
+		// logger를 이용해서 출력하는 방법
+		// trace - debug - info - warn - error
 		
-		//logger을 이용해서 출력하는 방법
-		//trace  - debug - info - warn - error
+		// debug : 개발의 흐름 파악 (이게 실행이 되었는지, 파라미터가 현재 무엇인지 확인할 때)
+		// info : 메소드 실행 파악
 		
-		//debug : 개발의 프름 파악
-		//info : 메소드 실행
-	
-		//이 필터가 생성이 되었다 라는 정보를 출력하고 싶다! info가 제일 적절함
+		// 이필터가 생성이 되었다 라는 정보를 출력하고 싶다! info 가 제일 적절함.
 		logger.info("초기화 필터 생성");
 	}
-       
-	//필터가 파괴될 때 실행(서버는 켜져있는데 백엔드 코드가 수정되었을 때)
-	public void destroy() {
-		
-		logger.info("초기화 필터 파괴");
 
-		
+	// 필터가 파괴될 때 실행 (서버는 켜져있는데 백엔드 코드가 수정되었을 때)
+	public void destroy() {
+		logger.info("초기화 필터 파괴");
 	}
 
-
+	
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		
 		// application 내장 객체 얻어오기
 		ServletContext application = request.getServletContext();
-				
-		//최상위 주소 얻어오기
-		String contextPath = ((HttpServletRequest)request).getContextPath(); // 다운캐스팅
-				
+		
+		// 최상위 주소 얻어오기
+		String contextPath =  ( (HttpServletRequest)request ).getContextPath();
+									// 다운캐스팅
 		// 세팅
 		application.setAttribute("contextPath", contextPath);
-				
-		// 연결된 다음 필터 수행(없으면 Servlet 수행)
+		
+		
 		chain.doFilter(request, response);
-
 	}
-
 
 
 }
--- src/main/java/edu/kh/comm/member/model/service/MemberService.java
+++ src/main/java/edu/kh/comm/member/model/service/MemberService.java
@@ -10,21 +10,23 @@ import edu.kh.comm.member.model.vo.Member;
  * 
  * 3. 클래스간의 결합도를 약화 시키기 위해서 -> 유지보수성 향상
  * 
- */
+ * */
+public interface MemberService {
 
-/*
- * 흩어진 관심사: 소스코드상에서 계속 반복해서 사용되는 부분들
- * ---> 유지보수를 어렵게 만든다.
- * 
- */
-
-
-public interface MemberService { 
-	// 모든 메서드가 추상 메소드(묵시적으로 public abstract)
-	// 모든 필드는 상수 (묵시적으로 public static final)
+	// 모든 메서드가 추상 메서드 (묵시적으로 public abstract) 
+	// 모든 필드는   상수        (묵시적으로 public static final)
+	
+	/** 로그인 서비스
+	 * @param inputMember
+	 * @return loginMember
+	 */
+	public abstract Member login(Member inputMember);
 	
 	
-//로그인 서비스
- public abstract Member login(Member inputMember); 
- 
+	
+	
+	
+	
+	
+	
 }
