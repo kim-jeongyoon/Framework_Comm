03f155ebf2f87a74c2b69e84f52c3395de11c59e
DESKTOP-HHMVOCR\JeongYoon <kjyoon811@gmail.com>
2023-04-30 23:40:00 +0900

    내정보수정 완료

--- src/main/java/edu/kh/comm/member/model/dao/MyPageDAO.java
+++ src/main/java/edu/kh/comm/member/model/dao/MyPageDAO.java
@@ -11,46 +11,70 @@ public class MyPageDAO {
 	
 	@Autowired
 	private SqlSessionTemplate sqlSession;
+	
 
-	/** 회원정보수정 DAO
+	/** 회원 정보 수정 DAO
 	 * @param paramMap
-	 * @return
+	 * @return result
 	 */
 	public int updateInfo(Map<String, Object> paramMap) {
 		
 		return sqlSession.update("myPageMapper.updateInfo", paramMap);
 	}
 
+
 	/** 현재 로그인한 회원의 암호화된 비밀번호 조회 DAO
 	 * @param memberNo
-	 * @return
+	 * @return encPw
 	 */
 	public String selectEncPw(int memberNo) {
 		
-		return sqlSession.selectOne("myPageMapper.selectEncPw",memberNo);
+		return sqlSession.selectOne("myPageMapper.selectEncPw", memberNo);
 	}
 
+
+	/** 비밀번호 변경 DAO
+	 * @param paramMap
+	 * @return result
+	 */
 	public int changePw(Map<String, Object> paramMap) {
 		
-		return sqlSession.update("myPageMapper.changcPw",paramMap);
+		return sqlSession.update("myPageMapper.changePw", paramMap);
 	}
 
-	/** 회원탈퇴 DAO
+
+	/** 회원 탈퇴 DAO
 	 * @param memberNo
-	 * @return
+	 * @return result
 	 */
 	public int secession(int memberNo) {
 		
-		return sqlSession.update("myPagemapper.secession", memberNo);
+		return sqlSession.update("myPageMapper.secession", memberNo);
 	}
+
 
 	/** 프로필 이미지 수정
 	 * @param map
-	 * @return
+	 * @return result
 	 */
 	public int updateProfile(Map<String, Object> map) {
 		
 		return sqlSession.update("myPageMapper.updateProfile", map);
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
--- src/main/java/edu/kh/comm/member/model/service/MyPageService.java
+++ src/main/java/edu/kh/comm/member/model/service/MyPageService.java
@@ -7,20 +7,28 @@ import edu.kh.comm.member.model.vo.Member;
 
 public interface MyPageService {
 
-	/** 회원정보수정 서비스
+	/** 회원 정보 수정 서비스
 	 * @param paramMap
-	 * @return
+	 * @return result
 	 */
 	int updateInfo(Map<String, Object> paramMap);
 
-	int chagePw(Map<String, Object> paramMap);
+	/** 비밀번호 변경 서비스
+	 * @param paramMap
+	 * @return result
+	 */
+	int changePw(Map<String, Object> paramMap);
 
 	/** 회원 탈퇴 서비스
 	 * @param loginMember
-	 * @return
+	 * @return result
 	 */
 	int secession(Member loginMember);
 
+	/** 프로필 이미지 수정 서비스
+	 * @param map
+	 * @return result
+	 */
 	int updateProfile(Map<String, Object> map) throws IOException;
 
 }
